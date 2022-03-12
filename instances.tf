5ed3410b1b7799273a2ae680ed6d78611ce35032
Olasoji <65076620+Sojstyles@users.noreply.github.com>
2022-03-12 13:27:40 -0500

    Update instances.tf
    Co-authored-by: Vladimir Mukhin <39741635+vladimirmukhin@users.noreply.github.com>

--- instances.tf
+++ instances.tf
@@ -2,9 +2,8 @@ resource "aws_instance" "myec2" {
   ami             = "ami-0742b4e673072066f"
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.TerraformEC2_security.name]
+  
   tags = {
     Name = "Terraform-Ec2"
   }
-
 }
-
