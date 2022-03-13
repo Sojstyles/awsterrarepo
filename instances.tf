3561fa74e7b07cca0a3dfd3d818b609f52e2887f
Vladimir Mukhin <39741635+vladimirmukhin@users.noreply.github.com>
2022-03-13 18:02:14 -0400

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "myec2" {
-  ami             = "ami-0742b4e673072066f"
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.TerraformEC2_security.name]
+  ami                    = "ami-0742b4e673072066f"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.TerraformEC2_security.name]
   
   tags = {
     Name = "Terraform-Ec2"
