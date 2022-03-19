222a7ca97e9dd8117b43a0e3490b2e558901f3aa
Techstellar <olasoji@olasojiopeyemi.com>
2022-03-18 22:09:02 -0400

    featureE branch

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "environemnt_code" {
   description = "What color code?"
   type        = string
 }
+
+variable "environemnt_code2" {
+  description = "What color code?"
+  type        = string
+}
