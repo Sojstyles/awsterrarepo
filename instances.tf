c24ce0e99d838918330c1d93939aab927a81a9c0
Techstellar <olasoji@olasojiopeyemi.com>
2022-03-21 12:47:53 -0400

    Added EIP and EC2 keypair

--- instances.tf
+++ instances.tf
@@ -3,10 +3,20 @@ resource "aws_instance" "myec2" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.TerraformEC2_security.id]
   subnet_id              = aws_subnet.public[0].id
+  key_name               = "awssecondkey"
 
   tags = {
     Name = "${var.environemnt_code}-public"
   }
+}
+
+resource "aws_eip" "elasticip" {
+  vpc = true
+}
+
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.myec2.id
+  allocation_id = aws_eip.elasticip.id
 }
 
 resource "aws_security_group" "TerraformEC2_security" {
