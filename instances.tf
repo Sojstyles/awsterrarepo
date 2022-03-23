b4a4bf51280ad51137f5d1f43d4df69cc64ec31e
Techstellar <olasoji@olasojiopeyemi.com>
2022-03-23 17:50:15 -0400

    added ec2 user data script

--- instances.tf
+++ instances.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "myec2" {
   vpc_security_group_ids = [aws_security_group.TerraformEC2_security.id]
   subnet_id              = aws_subnet.public[0].id
   key_name               = "thirdkey"
-
+  user_data              = file("ec2userfile.sh")
   tags = {
     Name = "${var.environemnt_code}-public"
   }
